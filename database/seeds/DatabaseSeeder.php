56c72ec1885b86aabdd1e3704b0222e6e81a626d
Josef <juarisco@outlook.com>
2020-01-05 17:35:32 -0500

    11.-Fun-with-Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(\App\User::class)->create([
+            'name' => 'John Doe',
+            'email' => 'john@doe.com'
+        ]);
+
+        factory(\App\User::class)->create([
+            'name' => 'Jane Doe',
+            'email' => 'jane@doe.com'
+        ]);
+
+        factory(\App\User::class)->create([
+            'name' => 'Mario Waelchi',
+            'email' => 'mario@waelchi.com'
+        ]);
     }
 }
