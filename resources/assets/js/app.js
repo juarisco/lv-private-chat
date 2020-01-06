9756f1aee1f34774094bed78184083343c6e3a65
Josef <juarisco@outlook.com>
2020-01-06 16:40:23 -0500

    19.-Check-this-Chat-Component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,8 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-// import ExampleComponent from './components/ExampleComponent.vue'
+Vue.component('chat-component', require('./components/ChatComponent.vue'));
+// import ExampleComponent from './components/ChatComponent.vue'
 
 const app = new Vue({
     el: '#app',
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
 
         <main class="py-4">
 {{--            @yield('content')--}}
-            <example-component></example-component>
+            <chat-component></chat-component>
         </main>
     </div>
 </body>
