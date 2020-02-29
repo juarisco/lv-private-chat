a41afe464be1000d7b914c99b5419106002bb18d
Josef <juarisco@outlook.com>
2020-02-29 11:29:37 -0500

    40.-Create-Event-and-Listener

---
+++ app/Events/SessionEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class SessionEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $session;
+    public $session_by;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param $session
+     * @param $session_by
+     */
+    public function __construct($session, $session_by)
+    {
+        //
+        $this->session = $session;
+        $this->session_by = $session_by;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('Chat');
+    }
+}
